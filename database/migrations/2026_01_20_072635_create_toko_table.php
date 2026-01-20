bd4605a7b5e6de7a58406212b26cbe969faa9eda
fajarfaturrahman2120 <fajarfaturrahman2120@gmail.com>
2026-01-20 14:30:02 +0700

    fix ae

---
+++ database/migrations/2026_01_20_072635_create_toko_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('toko', function (Blueprint $table) {
+            $table->id('id_toko');
+            $table->integer('id_owner');
+            $table->string('nama_toko');
+            $table->string('alamat_toko');
+            $table->string('hp_toko');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('toko');
+    }
+};
