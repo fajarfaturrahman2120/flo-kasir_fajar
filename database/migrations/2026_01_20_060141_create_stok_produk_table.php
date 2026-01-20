dcfe88f60e2a4bdd4e90b5967bd8c94a92993259
fajarfaturrahman2120 <fajarfaturrahman2120@gmail.com>
2026-01-20 14:02:13 +0700

    fix ae

--- database/migrations/2026_01_20_060141_create_stok_produk_table.php
+++ database/migrations/2026_01_20_060141_create_stok_produk_table.php
@@ -14,6 +14,17 @@ return new class extends Migration
         Schema::create('stok_produk', function (Blueprint $table) {
             $table->id('id_stok_produk');
             $table->integer('id_toko');
+            $table->integer('id_produk');
+            $table->date('tanggal_input');
+            $table->string('jenis_stok');
+            $table->string('jumlah_stok');
+            $table->string('harga_total_beli_stok');
+            $table->string('harga_satu_beli_stok');
+            $table->integer('id_supplier');
+            $table->string('status_bayar');
+            $table->string('pembayaran');
+            $table->text('keterangan');
+            $table->date('tanggal_expired');
             $table->timestamps();
         });
     }
